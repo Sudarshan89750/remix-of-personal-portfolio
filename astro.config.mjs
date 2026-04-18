957ba7ae74c711563abaefb4a949a3dd736d6894
sudarshan <sudarshan@wajooba.ai>
2026-04-18 10:11:01 +0530

    changes for prodcution

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
